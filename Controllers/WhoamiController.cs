0d05faebc53ce238ce84276ec0e5092111e8207c
Naoki Ikeguchi <root@siketyan.dev>
2020-08-08 23:28:47 +0900

    Add Whoami Controller

---
+++ Controllers/WhoamiController.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+namespace AAAuth.Controllers
+{
+    public class WhoamiResponse
+    {
+        public string Id { get; set; }
+        public string TenantId { get; set; }
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WhoamiController : ControllerBase
+    {
+        [HttpGet]
+        public WhoamiResponse Get()
+        {
+            return new WhoamiResponse
+            {
+                Id = User.GetObjectId(),
+                TenantId = User.GetTenantId(),
+                DisplayName = User.GetDisplayName(),
+                Name = User.FindFirstValue(ClaimConstants.Name),
+            };
+        }
+    }
+}
