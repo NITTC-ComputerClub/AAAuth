e7184c85ae7d87f851df3868778a27c3e801dd0e
Naoki Ikeguchi <root@siketyan.dev>
2020-08-08 23:28:30 +0900

    Replace Azure AD authentication with Microsoft.Identity

--- Startup.cs
+++ Startup.cs
@@ -6,12 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
 
 namespace AAAuth
 {
@@ -34,8 +33,7 @@ namespace AAAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.BearerAuthenticationScheme)
-                .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
+            services.AddMicrosoftWebAppAuthentication(Configuration);
             services.AddControllers();
         }
 
